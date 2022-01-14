883b917dda685ededea37d4377e487069663f81b
ARMAN UMAR <97071435+ARMANINFORMATIKA2021@users.noreply.github.com>
2022-01-14 08:13:38 +0800

    Belajar function

---
+++ function.py
@@ -0,0 +1,13 @@
+
+#Belajar Membuat Function
+a = 2
+b = 3
+def tambah():
+        hasil = a + b
+        print(f"Hasil {a} + {b} adalah {hasil}")
+def kurang():
+        hasil = a - b
+        print(f"Hasil {a} - {b} adalah {hasil}")
+#memanggil function
+tambah()
+kurang()
