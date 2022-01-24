24d0725ca9f6e62025c1f914c93db6c523449c79
ARMAN UMAR <97071435+ARMANINFORMATIKA2021@users.noreply.github.com>
2022-01-24 09:22:28 +0800

    Membuat Variabel

--- PHP/DASAR PHP/variabel.php
+++ PHP/DASAR PHP/variabel.php
@@ -1,1 +1,10 @@
+<?php
+//Belajar membuat variabel
+$nama = "ARMAN";
+$nim = "D0221049";
+$jurusan = "TEKNIK INFORMATIKA";
+$angkatan = 2021;
 
+//Mencetak nilai varuabel
+echo("NAMA SAYA $nama, NIM $nim <br>JURUSAN $jurusan, ANGKATAN $angkatan");
+?>
