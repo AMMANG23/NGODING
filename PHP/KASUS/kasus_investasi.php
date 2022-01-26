2f69bfd146794d07073a6d25fe6b55e4a4682f11
ARMAN UMAR <97071435+ARMANINFORMATIKA2021@users.noreply.github.com>
2022-01-26 13:34:05 +0800

    Kasus 1

--- PHP/KASUS/kasus_investasi.php
+++ PHP/KASUS/kasus_investasi.php
@@ -1,1 +1,19 @@
+<?php
+/*
+Kaco bellu berinvestasi sebesar Rp. 100 dengan bunga 10%. Oleh karena
+itu setiap tahun investasi tersebut akan menghasilkan keuntungan
+sebesar 10% dari investasi semula, atau Rp. 10 setiap tahun. Tuliskan
+suatu program untuk menghitung berapa waktu yang dibutuhkan agar
+keuntungan kaco bellu dari bunga menjadi  Rp.1000.
 
+Ada yg bisa?
+*/
+$investasi = 100;
+$bunga = 10/100;
+$tahun = 0;
+while($investasi < 1000){
+  $keuntungan = $investasi * $bunga;
+  $investasi += $keuntungan;
+  $tahun += 1;
+  echo("<br>investasi tahun $tahun, untung $keuntungan");
+}
