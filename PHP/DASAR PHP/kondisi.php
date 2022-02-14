71503bc4ce2976364e89921b13a383e41f5d5d44
ARMAN UMAR <97071435+AMMANG23@users.noreply.github.com>
2022-02-14 21:52:16 +0800

    Membuat kondisi

---
+++ PHP/DASAR PHP/kondisi.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+   <title>Membuat kondisi</title>
+</head>
+
+<body>
+   <div class="container">
+      <form action="" method="post" class="col-4 mt-4">
+         <div class="mb-3">
+            <h3><label for="exampleInputEmail1" class="form-label">Input Student Grades</label></h3>
+            <input name="value" type="number" class="form-control">
+         </div>
+         <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+      </form>
+      <?php
+      if (isset($_POST['submit'])) {
+         if ($_POST['value'] >= 70 && $_POST['value'] <= 100) { ?>
+            <div class="alert alert-primary col-4 mt-4" role="alert">
+               <?php echo ("Selamat Nilai Anda Memenuhi Standar Ketentuan") ?>
+            </div>
+         <?php } elseif ($_POST['value'] >= 50 && $_POST['value'] < 70) { ?>
+            <div class="alert alert-primary col-4 mt-4" role="alert">
+               <?php echo ("Selamat Nilai Anda Lumayan Bagus") ?>
+            </div> <?php }
+               } ?>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
