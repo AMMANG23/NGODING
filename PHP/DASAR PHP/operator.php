319a02491542b9667d6ab3725f5c5f08d9ea60ce
ARMAN UMAR <97071435+AMMANG23@users.noreply.github.com>
2022-02-15 21:11:00 +0800

    Operator Aritmatika

---
+++ PHP/DASAR PHP/operator.php
@@ -0,0 +1,9 @@
+<?php
+$angka1 = 10;
+$angka2 = 5;
+
+echo $angka1 + $angka2 . "<br>";
+echo $angka1 - $angka2 . "<br>";
+echo $angka1 * $angka2 . "<br>";
+echo $angka1 / $angka2 . "<br>";
+echo $angka1 % $angka2;
